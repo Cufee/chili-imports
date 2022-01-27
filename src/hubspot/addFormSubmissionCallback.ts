8855b6a407b0daf50058a4a28f36dab7d073ff26
Cufee <vkouzin@outlook.com>
2022-01-27 16:47:17 -0500

    removed reduce

--- src/hubspot/addFormSubmissionCallback.ts
+++ src/hubspot/addFormSubmissionCallback.ts
@@ -1,7 +1,14 @@
 export async function AddFormSubmissionCallback(callback: (data: Record<string, any>, context: Record<string, any>) => Promise<void>) {
   window.addEventListener("message", async (event) => {
     if (event.data.type === "hsFormCallback" && event.data.eventName === "onFormSubmitted") {
-      const data: Record<string, any> = event?.data?.data?.reduce((obj: Record<string, any>, item: Record<string, any>) => Object.assign(obj, { [item.name]: item.value }), {});
+      const data: Record<string, any> = {};
+      if (typeof event.data.data === "object") {
+        for (const key in event.data.data) {
+          if (event.data.data.hasOwnProperty(key)) {
+            data[key] = event.data.data[key];
+          }
+        }
+      }
       const contextInput = document.querySelector('input[name="hs_context"]') as HTMLInputElement;
       const context = contextInput?.value ? JSON.parse(contextInput.value) : {};
       await callback(data, context);
