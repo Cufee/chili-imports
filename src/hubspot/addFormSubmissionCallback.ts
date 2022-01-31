4f9f629d0612a3f5afc6a070b6fb2fad76c4ae0b
Cufee <vkouzin@outlook.com>
2022-01-31 13:06:31 -0500

    fixed hubspot callbacks data

--- src/hubspot/addFormSubmissionCallback.ts
+++ src/hubspot/addFormSubmissionCallback.ts
@@ -1,16 +1,16 @@
 export async function AddFormSubmissionCallback(callback: (data: Record<string, any>, context: Record<string, any>) => void): Promise<void> {
-  window.addEventListener("message", async (event) => {
-    if (event.data.type === "hsFormCallback" && event.data.eventName === "onFormSubmitted") {
+  window.addEventListener("message", (event) => {
+    if (event.data.type === "hsFormCallback" && event.data.eventName === "onFormSubmit") {
       const data: Record<string, any> = {};
       if (typeof event.data.data === "object") {
         for (const key in event.data.data) {
-          if (event.data.data.hasOwnProperty(key)) {
-            data[key] = event.data.data[key];
-          }
+          data[event.data.data[key].name] = event.data.data[key].value;
         }
       }
+
       const contextInput = document.querySelector('input[name="hs_context"]') as HTMLInputElement;
       const context = contextInput?.value ? JSON.parse(contextInput.value) : {};
+      context.formId = event.data.id;
       callback(data, context);
     }
   });
--- src/hubspot/addFormSubmitRedirectParams.ts
+++ src/hubspot/addFormSubmitRedirectParams.ts
@@ -1,12 +1,11 @@
-import DebugLog from "../common/debugLog";
 import { AddFormSubmissionCallback } from "./addFormSubmissionCallback";
+import DebugLog from "../common/debugLog";
 
 async function AddFormSubmitRedirectParams(redirectUrl: string, debug = true): Promise<void> {
   AddFormSubmissionCallback((data: Record<string, any>, context: Record<string, any>) => {
     const formId = context.formId;
     DebugLog(debug, `Form ID ${formId} submitted`);
     DebugLog(debug, `Redirecting to ${redirectUrl}`);
-
     DebugLog(debug, `Data: ${JSON.stringify(data)}`);
 
     const params = new URLSearchParams(data);
